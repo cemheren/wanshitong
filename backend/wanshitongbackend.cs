82c733cfd48c150d82ed5154bfb15b4c9ebf0e03
cemheren <cemheren@gmail.com>
2019-11-26 13:39:38 -0800

    handler pattern

--- backend/wanshitongbackend.cs
+++ backend/wanshitongbackend.cs
@@ -19,15 +19,35 @@ namespace wanshitong.backend
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string api = req.Query["api"];
+
+            switch (api)
+            {
+                case "newkey":
+                    return await HandleNewKey(req);
+                default:
+                    return await HandleNotFound(api);
+            }
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var name = data?.name;
 
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+
+        private static async Task<IActionResult> HandleNewKey(HttpRequest req)
+        {
+
+            
+            return (ActionResult)new OkObjectResult($"{Guid.NewGuid()}");
+        }
+
+        private static async Task<IActionResult> HandleNotFound(string route)
+        {
+            return new NotFoundObjectResult($"Route {route} is not found");
         }
     }
 }
